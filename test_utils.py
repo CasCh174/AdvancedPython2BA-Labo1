2d33195347d6b3bf13e611cdfd480131130f4ced
Charles CASTERMANS <14094@ecam.be>
2016-02-03 16:03:08 +0100

    deuxieme changement

--- test_utils.py
+++ test_utils.py
@@ -1,4 +1,4 @@
-# test_utils.py
+#gçit test_utils.py
 # Author: Sébastien Combéfis
 # Version: February 2, 2016
 
@@ -24,3 +24,4 @@ if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
     runner = unittest.TextTestRunner()
     exit(not runner.run(suite).wasSuccessful())
+#pour le push
